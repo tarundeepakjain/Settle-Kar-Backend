c9790bc7d6fa697d06df01703271f236a433fe0f
avadhesh11 <202452309@iiitvadodara.ac.in>
2025-09-15 12:32:10 +0530

    minor changes

--- src/routes/group.js
+++ src/routes/group.js
@@ -7,5 +7,7 @@ router.post("/new", GroupController.createGroup);
 router.post("/add-member", GroupController.addMember);
 router.delete("/delete-member", GroupController.deleteMember);
 // router.delete("/delete", GroupController.deleteGroup);
-
+// router.post("/invite",async(req,res)=>{
+    
+// })
 export default router;
--- src/routes/prsnl.js
+++ src/routes/prsnl.js
@@ -1,8 +1,11 @@
 import express from "express";
 import multer from "multer";
+import prsnl from "../models/prsnl.js";
+import user from "../models"
 const router=express.Router();
 
 router.post('/',async(req,res)=>{
+const {uid,expense}=req.body;
 
 })
 
