9c961aff6583bd05481ea78a8f6c43055eda760b
avadhesh11 <202452309@iiitvadodara.ac.in>
2025-10-07 01:04:23 +0530

    cors updated

--- server.js
+++ server.js
@@ -11,12 +11,23 @@ const app=express();
 app.use(express.json());
 const PORT=process.env.PORT || 5001;
 
-const allowedOrigin = 'http://localhost:8081';
+
+const allowedOrigins = [
+  "http://localhost:8081", // Expo web
+  "exp://127.0.0.1:19000"  // Expo Go dev URL (optional)
+];
 
 app.use(cors({
-  origin: "*", 
-  methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: function(origin, callback){
+    if(!origin || allowedOrigins.includes(origin)){
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ["GET","POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 mongo();
 app.use('/',home);
