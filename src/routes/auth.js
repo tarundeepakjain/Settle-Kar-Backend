89c8241adba24404158dbdb9504f4c7ad84baa7c
avadhesh11 <202452309@iiitvadodara.ac.in>
2025-11-17 16:34:05 +0530

    otp auth deploy correction

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,7 +10,8 @@ const router = express.Router();
 const otpStore = new Map();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port:587,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
